b2c8af9dd3672499db47df0c959f8881ae1ab743
Onkar9595 <111443758+Onkar9595@users.noreply.github.com>
2022-11-09 18:18:17 -0500

    wip

--- Assets/Scripts/TaskComplete.cs
+++ Assets/Scripts/TaskComplete.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,14 @@ public class TaskComplete : SelectionLogic
 {
     [SerializeField] private Button _selectedButton1;
     [SerializeField] private Button _selectedButton2;
-    
-    public void ContextComplete()
+
+    public void Update()
     {
         if (_selectedButton1.image.sprite == _winningSprite && _selectedButton2.image.sprite == _winningSprite)
         {
             Debug.Log("You won");
         }
     }
-  
+
+    
 }
