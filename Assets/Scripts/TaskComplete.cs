0796c95495045346c2d9d5b1e28bd3a2c530b46b
Onkar9595 <111443758+Onkar9595@users.noreply.github.com>
2022-11-10 04:35:12 -0500

    wip

--- Assets/Scripts/TaskComplete.cs
+++ Assets/Scripts/TaskComplete.cs
@@ -9,14 +9,38 @@ public class TaskComplete : SelectionLogic
 {
     [SerializeField] private Button _selectedButton1;
     [SerializeField] private Button _selectedButton2;
+    [SerializeField] private Canvas potato;
+    [SerializeField] private Canvas onion;
+    [SerializeField] private Canvas orange;
+    [SerializeField] private Canvas washing;
+    
+    [SerializeField] private Canvas contextDone;
+    [SerializeField] private Canvas lightmango;
+    [SerializeField] private Canvas lightknife;
 
+    public void Start()
+    {
+        contextDone.enabled = false;
+        lightknife.enabled = false;
+        lightmango.enabled = false;
+    }
     public void Update()
     {
         if (_selectedButton1.image.sprite == _winningSprite && _selectedButton2.image.sprite == _winningSprite)
         {
+            contextDone.enabled = true;
+            lightknife.enabled = true;
+            lightmango.enabled = true;
+            potato.enabled = false;
+            onion.enabled = false;
+            orange.enabled = false;
+            washing.enabled = false;
             Debug.Log("You won");
         }
     }
 
-    
+   
+
+
+
 }
